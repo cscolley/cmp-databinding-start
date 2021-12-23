586a543583289e9d03b7c1bba04cc38fa281e5cc
cscolley <chris.s.colley@gmail.com>
2021-12-23 14:36:37 +1100

    Finished Udemy Section 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [];
-  newServerName = '';
-  newServerContent = '';
 
-  onAddServer() {
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
     this.serverElements.push({
       type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
+      name: serverData.serverName,
+      content: serverData.serverContent
     });
   }
 
-  onAddBlueprint() {
+  onBlueprintAdded(blueprintData: {serverName: String, serverContent: string}) {
     this.serverElements.push({
       type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
     });
+  }
+
+  onDelete() {
+    this.serverElements = [];
   }
 }
